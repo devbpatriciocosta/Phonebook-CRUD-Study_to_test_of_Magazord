fd732e5492e5e3dd003e5b31b620994cfb7594e5
Bruno Patrício <bpatriciocosta@gmail.com>
2023-10-10 11:53:20 -0300

    Login and Logout Pages

---
+++ address-book/logout.php
@@ -0,0 +1,16 @@
+<?php
+
+// Start the session to access session variables
+session_start();
+
+// Unset the 'user_id' / 'name' session variable
+unset($_SESSION['user_id']);
+unset($_SESSION['name']);
+
+// Destroy the session
+session_destroy();
+
+// Redirect the user to the login page
+header("Location: login.php");
+
+?>
